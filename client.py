17026fc268f5d102378b6ea9a0f11d33ed8e02d0
rpaunero <r.paunero@alumnos.urjc.es>
2015-11-23 18:08:23 +0100

    Pep8

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ import sys
 if len(sys.argv) != 3:
     sys.exit('Usage: python client.py method receiver@IP:SIPport')
 
-METHOD = sys.argv[1]    
+METHOD = sys.argv[1]
 LOGIN = sys.argv[2].split(':')[0]
 IP = sys.argv[2].split(':')[0].split('@')[1]
 PORT = int(sys.argv[2].split(':')[1])
@@ -24,7 +24,7 @@ if not '@' or ':' in LOGIN:
 
 if METHOD in ['INVITE', 'BYE']:
     LINE = METHOD
-LINE = LINE + ' ' + 'sip:' + LOGIN + ' '+ 'SIP/2.0'
+LINE = LINE + ' ' + 'sip:' + LOGIN + ' ' + 'SIP/2.0'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -47,8 +47,9 @@ if METHOD == 'INVITE':
     print(instruccion2)
     instruccion3 = instruccion.split()[7]
     print(instruccion3)
-    if instruccion1 == '100' and instruccion2 == '180' and instruccion3 == '200':
-        LINE = 'ACK' + ' ' + 'sip:' + LOGIN + ' '+ 'SIP/2.0'
+    if instruccion1 == '100' and instruccion2 == '180'
+    and instruccion3 == '200':
+        LINE = 'ACK' + ' ' + 'sip:' + LOGIN + ' ' + 'SIP/2.0'
         print("Enviando: " + LINE)
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
         data = my_socket.recv(1024)
