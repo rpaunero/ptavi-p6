6a39198f2abf104500980584503ff19730053338
rpaunero <r.paunero@alumnos.urjc.es>
2015-11-23 11:04:04 +0100

    ENVIANDO METHODS

--- client.py
+++ client.py
@@ -5,20 +5,32 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+if len(sys.argv) != 3:
+    sys.exit('Usage: python client.py method receiver@IP:SIPport')
+
+METHOD = sys.argv[1]    
+LOGIN = sys.argv[2].split(':')[0]
+IP = sys.argv[2].split(':')[0].split('@')[1]
+PORT = int(sys.argv[2].split(':')[1])
+
+if not '@' or ':' in LOGIN:
+    sys.exit('Usage: python client.py method receiver@IP:SIPport')
+
+if METHOD in ['INVITE', 'ACK', 'BYE']:
+    LINE = METHOD
+LINE = LINE + ' ' + 'sip:' + LOGIN + ' '+ 'SIP/2.0'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
+
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
